9721e83e87e2fe2248100cf15db7611134a8681d
Kimbeomju <64351062+K-beomju@users.noreply.github.com>
2022-01-12 12:09:40 +0900

    적 푸쉬

--- Assets/Scripts/Controll/Enemy/Enemy.cs
+++ Assets/Scripts/Controll/Enemy/Enemy.cs
@@ -8,12 +8,16 @@ namespace Prototype
     public class Enemy : EnemyPrototype
     {
         public int hp;
-        public int speed;
+        private int maxHp = 100;
+        public int speed = 3;
+        //public EnemyHPBar hpBar;
+        public Vector3 offset;
+
 
 
         public Enemy(int hp, int speed)
         {
-            this.hp = hp;
+            this.maxHp = hp;
             this.speed = speed;
 
         }
@@ -23,10 +27,32 @@ namespace Prototype
             return new Enemy(hp, speed);
         }
 
+        private void OnEnable()
+        {
+            hp = maxHp;
+          //  hpBar.Reset(ScreenTransform(offset), 1);
+          
+        }
+
+        //public Vector3 ScreenTransform(Vector3 Correction)
+        //{
+        //    Vector3 pos = Camera.main.WorldToScreenPoint(transform.position + Correction);
+        //    return pos;
+        //}
+
+        private void Update()
+        {
+            Move();
+
+            //hpBar.SetValue(hp / maxHp);
+            //hpBar.SetPosition(ScreenTransform(offset));
+        }
+
 
         public override void Move()
         {
 
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
         }
 
         public override void Attack()
--- Assets/Scripts/UI/UIRootGame.cs
+++ Assets/Scripts/UI/UIRootGame.cs
@@ -13,17 +13,17 @@ public class UIRootGame : MonoBehaviour
         GameSceneClass.gUiRootGame = this;
     }
 
-    private void Update() 
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            List<string> keyList = new List<string>(Global.spritesDic.Keys);
-            int randomIdx = Random.Range(0, keyList.Count - 1);
+    //private void Update() 
+    //{
+    //    if(Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        List<string> keyList = new List<string>(Global.spritesDic.Keys);
+    //        int randomIdx = Random.Range(0, keyList.Count - 1);
             
-            testImage.sprite = Global.spritesDic[keyList[randomIdx]];
-            testImage.SetNativeSize();
-        }
-    }
+    //        testImage.sprite = Global.spritesDic[keyList[randomIdx]];
+    //        testImage.SetNativeSize();
+    //    }
+    //}
 
     public void TestFunc()
     {
