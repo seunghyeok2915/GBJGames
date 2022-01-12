7d9818e832e22666a4cd841721e4e19b5bf5b334
Kimbeomju <64351062+K-beomju@users.noreply.github.com>
2022-01-12 13:38:23 +0900

    ㅁㄴㅇ

---
+++ Assets/Scripts/UI/GameSpeedBtn.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSpeedBtn : MonoBehaviour
+{
+    private int gameSpeed;
+    public Text gameSpeedTxt;
+    public Button gameSpeedBtn;
+
+    private bool fire = false;
+
+    private void Awake()
+    {
+        gameSpeedBtn.onClick.AddListener(() => UpSpeed());
+    }
+
+    public void UpSpeed()
+    {
+       if(!fire)
+        {
+            Time.timeScale = 2f; 
+            fire = true;
+        }
+       else
+        {
+            Time.timeScale = 1f;
+            fire = false;
+        }
+        gameSpeedTxt.text = string.Format("X{0}", Time.timeScale);
+
+
+    }
+}
